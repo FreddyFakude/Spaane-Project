15cd2c2f5d066a697f776bc1ddc8fdfdb17787d6
jerryboy <jerry@taxtim.com>
2023-08-25 00:46:01 +0200

    updated seeder

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -32,7 +32,7 @@ class CompanySeeder extends Seeder
                 'id' => 2,
                 'head_office_name' => "King Show Business 2",
                 'name' => "King Show Business 2",
-                'phone_number' => '27788129192',
+                'phone_number' => '27788129193',
                 'date_creation' => '2022-01-01',
                 'status'=> Company::STATUS[2],
                 'fiscal_year_start' => '2022-01-01',
